114c0d2b4e2e18bf0dddf94a6f67ddd2259a42e6
tlhomphogit <tlhomphosuper@gmail.com>
2026-02-05 16:00:09 +0200

    Refactor performance analyzer: update file reading method and enhance data handling

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,32 @@
+import os
+import csv
+
 class PerfomanceAnalyser():
     def __init__(self):
-        pass
+        self.student_data = []
 
     def read_from_file(self, filename):
-        with open(filename, 'r') as file:
-            student_results = file.read()
+        if not os.path.exists(filename):
+            raise FileNotFoundError(f'Error! The file {filename} was not found.')
+
+        with open(filename, mode='r') as file:
+            student_results = csv.DictReader(file)
+        
+            for row in student_results:
+                row['score'] = int(row['score'])
+                self.student_data.append(row)
+                
+    def __str__(self):
+        return f'{self.student_data}'
+    
+    def __repr__(self):
+        pass
+
+def main():
+    filename = 'student_results.csv'
+    perfomance = PerfomanceAnalyser()
+    perfomance.read_from_file(filename)
+    print(perfomance)
+
+if __name__ == '__main__':
+    main()
